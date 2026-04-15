8929ffbd8d638f41c48bdb5bad1436b642986b9a
Juan Pablo <54999552+JPpellegrini@users.noreply.github.com>
2026-04-15 20:36:53 -0300

    Test

---
+++ Socios.Infrastructure/Migrations/20260415233605_TestCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Socios.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestCheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "Id_Usuario",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$5.O9NB.FFBZ98GrE24jq7et8c0ACOkRVsSueihm78or/JNZmjVGay");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "Id_Usuario",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$wH6QnQnQnQnQnQnQnQnQOeQnQnQnQnQnQnQnQnQnQnQnQnQnQnQ");
+        }
+    }
+}
