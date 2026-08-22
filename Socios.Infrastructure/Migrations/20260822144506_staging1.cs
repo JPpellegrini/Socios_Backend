d74eb04613bd35d1930be6148cad7c4672dc9417
Juan Pablo <54999552+JPpellegrini@users.noreply.github.com>
2026-08-22 12:00:23 -0300

    Migraciones

---
+++ Socios.Infrastructure/Migrations/20260822144506_staging1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Socios.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class staging1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "contactos",
+                keyColumn: "Id_Contacto",
+                keyValue: 4,
+                column: "Tipo",
+                value: "TELÉFONO");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "contactos",
+                keyColumn: "Id_Contacto",
+                keyValue: 4,
+                column: "Tipo",
+                value: "Emergencia");
+        }
+    }
+}
